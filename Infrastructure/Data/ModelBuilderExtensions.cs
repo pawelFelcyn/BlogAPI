0b80e87091db8001c7ef2ee8c8c3155ce332d0cb
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-16 18:10:01 +0100

    Added User entity and DbContext

---
+++ Infrastructure/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder OnUserCreating(this ModelBuilder builder)
+    {
+        builder
+    }
+}
