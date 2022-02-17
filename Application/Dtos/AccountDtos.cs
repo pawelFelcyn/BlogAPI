a5a5d4d4d8eef80a10ac06cb6851392bf864268f
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-17 11:49:52 +0100

    Added LoginDto and RegisterDto

---
+++ Application/Dtos/AccountDtos.cs
@@ -0,0 +1,4 @@
+﻿namespace Application.Dtos;
+
+public record LoginDto(string Email, string Password);
+public record RegisterDto(string FirstName, string LastName, string Nick, string Role, string Email, string Password, string ConfirmPassword);
