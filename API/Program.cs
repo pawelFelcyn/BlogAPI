79ab357c1f41f1b30c1b2b6fcb000b0d8a525783
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-18 10:08:44 +0100

    Added Authentication section to appsettings.Development.json and configured authentication in Program

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
 using API.Middleware;
 using Application;
+using Application.Authentication;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,22 @@ builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(bui
 builder.Services.AddValidators().AddServices();
 builder.Services.AddAutoMapper().AddHelpers().AddRepositories();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+var authenticationSettings = new AuthenticationSettings();
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+builder.Services.AddSingleton(authenticationSettings);
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(cfg =>
+    {
+        cfg.RequireHttpsMetadata = false;
+        cfg.SaveToken = true;
+        cfg.TokenValidationParameters = new()
+        {
+            ValidAudience = authenticationSettings.JwtIssuer,
+            ValidIssuer = authenticationSettings.JwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,7 +49,7 @@ app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
