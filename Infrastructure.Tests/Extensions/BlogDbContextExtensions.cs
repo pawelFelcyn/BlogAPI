242f14b39b5dc77d53ae8acb2ae9c934e9b76f8d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-18 12:06:16 +0100

    Added AccountRepositoryTests with tests og GetByEmail method

---
+++ Infrastructure.Tests/Extensions/BlogDbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Tests.Extensions;
+
+internal static class BlogDbContextExtensions
+{
+    public static User SeedUser(this BlogDbContext dbContext)
+    {
+        var user  = new User()
+        {
+            Email = "email",
+            FirstName = "",
+            LastName = "",
+            Nick = "",
+            Role = "",
+            PasswordHash = ""
+        };
+
+        dbContext.Users.Add(user);
+        dbContext.SaveChanges();
+
+        return user;
+    }
+}
---
+++ Infrastructure.Tests/Repositories/AccountRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entities;
+using Domain.Exceptions;
+using FluentAssertions;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Infrastructure.Tests.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xunit;
+
+namespace Infrastructure.Tests.Repositories;
+
+public class AccountRepositoryTests 
+{
+    private readonly AccountRepository _repository;
+    private readonly BlogDbContext _context;
+    
+    public AccountRepositoryTests()
+    {
+        var builder = new DbContextOptionsBuilder();
+        //var modelBuilder = new ModelBuilder();
+        //modelBuilder.Entity<User>();
+        //builder.UseModel(modelBuilder.FinalizeModel());
+        builder.UseInMemoryDatabase("BlogDb");
+        _context = new BlogDbContext(builder.Options);
+        _repository = new AccountRepository(_context);
+    }
+
+    [Fact]
+    public void GetByEmail_ForCorrentEmail_ReturnsProperUser()
+    {
+        var user = _context.SeedUser();
+
+        var result = _repository.GetByEmail(user.Email);
+
+        result.Should().Be(user);
+    }
+
+    [Fact]
+    public void GetByEmail_ForIncorrectEmail_ThrowsInvalidEmailException()
+    {
+        var action = () => _repository.GetByEmail("invalid email");
+
+        Assert.Throws<InvalidEmailException>(action);
+    }
+}
