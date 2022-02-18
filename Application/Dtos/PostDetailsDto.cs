1822b03741a32eb5ef440aa59961d70b3941892f
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-18 17:11:19 +0100

    Created Post dtos and IPostService

---
+++ Application/Dtos/PostDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos;
+
+public record PostDetailsDto
+{
+    public int Id { get; set; }
+    public string? Title { get; init; }
+    public string? Content { get; init; }
+    public DateTime LastModyfied { get; init; }
+    public DateTime Created { get; init; }
+}
---
+++ Application/Dtos/PostDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos;
+
+public record PostDto
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public DateTime LastModyfied { get; set; }
+}
