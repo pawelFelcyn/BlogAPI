a177a4725de0da0b4c2abfa5152a60a63f37e5e8
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-19 14:26:41 +0100

    Added UpdatePostDtoValidator

---
+++ Application/Validators/UpdatePostDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class UpdatePostDtoValidator : AbstractValidator<UpdatePostDto>
+{
+    public UpdatePostDtoValidator()
+    {
+        RulesForContent();
+    }
+
+    private void RulesForContent()
+    {
+        RuleFor(u => u.Content)
+            .NotEmpty()
+            .WithMessage("Field 'Content' must not be empty")
+            .MaximumLength(5000)
+            .WithMessage("The maximum length of the value in field 'Content' is 5000 characters");
+    }
+}
