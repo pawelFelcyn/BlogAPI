87205d923a16aac23a4350ae34d75f3957e89ad5
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-25 21:12:11 +0100

    Added COmment Entity

---
+++ Domain/Entities/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string? Content { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastModyfied { get; set; }
+    public int CreatedById { get; set; }
+    public User? CreatedBy { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
