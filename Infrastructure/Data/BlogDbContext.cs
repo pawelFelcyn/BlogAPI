14934553e077ef9c623baaa882d1475386139536
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-25 21:23:38 +0100

    Added invoking of OnCommentCreating in the constructor

--- Infrastructure/Data/BlogDbContext.cs
+++ Infrastructure/Data/BlogDbContext.cs
@@ -7,6 +7,7 @@ public class BlogDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Post> Posts { get; set; }
+    DbSet<Comment> Comments { get; set; }
 
     public BlogDbContext(DbContextOptions options) : base(options)
     {
@@ -15,6 +16,7 @@ public class BlogDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.OnUserCreating()
-                    .OnPostCraeting();
+                    .OnPostCraeting()
+                    .OnCommentCreating();
     }
 }
