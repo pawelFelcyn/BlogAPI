37936f35b656ef3d773fa4b5ebbb0e234583ed28
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-26 15:23:06 +0100

    Made Comments collection public in BlogDbContext

--- Infrastructure/Data/BlogDbContext.cs
+++ Infrastructure/Data/BlogDbContext.cs
@@ -7,7 +7,7 @@ public class BlogDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Post> Posts { get; set; }
-    DbSet<Comment> Comments { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 
     public BlogDbContext(DbContextOptions options) : base(options)
     {
