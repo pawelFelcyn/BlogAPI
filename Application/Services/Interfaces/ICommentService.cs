b331a0e4957a645c9459aa56e670b6a8f0736467
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-26 11:02:57 +0100

    Added ICommentService with CRUD methods

---
+++ Application/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dtos;
+
+namespace Application.Services;
+
+public interface ICommentService
+{
+    IEnumerable<CommentDto> GetAll(int postId);
+    CommentDto GetById(int postId, int commentId);
+    CommentDto Create(int postId, CreateCommentDto dto);
+    CommentDto Update(int postId, int commentId, UpdateCommentDto dto);
+    void Delete(int postId, int commentId);
+}
