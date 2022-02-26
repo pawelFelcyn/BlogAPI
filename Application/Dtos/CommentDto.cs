4db7efa589fbfddb0cb1e84fcaa60ffcf67e47ff
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-02-26 10:59:48 +0100

    Added comment dtos

---
+++ Application/Dtos/CommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dtos;
+
+public record CommentDto
+{
+    public int Id { get; init; }
+    public string? CreatedByNick { get; set; }
+    public DateTime LastModyfied { get; set; }
+    public string Content { get; set; }
+}
